f7665f84bbffb5a282b30d8c4865dc957bc26d7f
mit018 <80405683+mit018@users.noreply.github.com>
2021-09-05 04:49:25 -0300

    Add files via upload

--- tps/entrada e saida/tp01/TP01Q15 - RECURSIVO - Is em Java/TP01Q15.java
+++ tps/entrada e saida/tp01/TP01Q15 - RECURSIVO - Is em Java/TP01Q15.java
@@ -16,7 +16,7 @@ class TP01Q15 {
         MyIO.print(" NAO");
       }
 
-      if (isInt(str, 0)) {
+      if (isInt(str, 0, 0)) {
         MyIO.print(" SIM");
       } else {
         MyIO.print(" NAO");
@@ -35,14 +35,16 @@ class TP01Q15 {
   }
 
   public static boolean isFim(String s) {
-    return (
-      s.length() == 3 &&
-      s.charAt(0) == 'F' &&
-      s.charAt(1) == 'I' &&
-      s.charAt(2) == 'M'
-    );
+    return (s.length() == 3 && s.charAt(0) == 'F' && s.charAt(1) == 'I' && s.charAt(2) == 'M');
   }
 
+  /**
+   * Testa se a string por apenas vogais
+   * 
+   * @param s -> string a ser testada
+   * @param i -> posicao da string
+   * @return -> true ou false
+   */
   public static boolean allVogal(String s, int i) {
     char[] vogais = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
 
@@ -59,6 +61,13 @@ class TP01Q15 {
     return false;
   }
 
+  /**
+   * Testa se a string por apenas consoantes
+   * 
+   * @param s -> string a ser testada
+   * @param i -> posicao da string
+   * @return -> true ou false
+   */
   public static boolean allCons(String s, int i) {
     char[] vogais = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
 
@@ -68,69 +77,118 @@ class TP01Q15 {
 
     for (int j = 0; j < 10; j++) {
       if (s.charAt(i) == vogais[j] || s.charAt(i) < 65) {
-        return false;
+        return false; // se em alguma posicao for uma vogal ou for algo diferente de uma letra, nao eh
+                      // uma consoante
       }
     }
     return allCons(s, i + 1);
   }
 
-  public static boolean isInt(String s, int i) {
-    int pon = 0;
-
+  /**
+   * Testa se a string eh um inteiro
+   * 
+   * @param s -> string a ser testada
+   * @return -> true ou false
+   */
+  public static boolean isInt(String s, int pon, int i) {
     if (i == s.length()) {
       return true;
     }
 
-    if (!(s.charAt(i) < 48 || s.charAt(i) > 57)) {
-      
+    if (s.charAt(i) < 48 || s.charAt(i) > 57) {
+      if (s.charAt(i) == '.') {// se nao for um numero, pode ser um ponto
+        pon++;
+        if (pon > 1) {
+          // se houver mais de um ponto, o segundo ponto deve ser precedido
+          // de no minimo 3 numeros e outro ponto e logo depois devem havem outros 3
+          // numeros (ex: 1.000.000 e nao 1.000.)
+          if (s.charAt(i - 1) < 48 || s.charAt(i - 1) > 57) {
+            return false;
+          } else if (s.charAt(i - 2) < 48 || s.charAt(i - 2) > 57) {
+            return false;
+          } else if (s.charAt(i - 3) < 48 || s.charAt(i - 3) > 57) {
+            return false;
+          } else if (s.charAt(i - 4) != '.') {
+            return false;
+          } else if (s.length() < i + 4) {
+            return false;
+          } else if (s.charAt(i + 1) < 48 || s.charAt(i + 1) > 57) {
+            return false;
+          } else if (s.charAt(i + 2) < 48 || s.charAt(i + 2) > 57) {
+            return false;
+          } else if (s.charAt(i + 3) < 48 || s.charAt(i + 3) > 57) {
+            return false;
+          }
+        }
+      } else {
+        return false;
+      }
     }
 
-    if (pon == 1) {
+    if (pon == 1) { // se houver apenas um ponto, nao eh inteiro (ex: 1.0)
       return false;
     }
 
-    return isInt(s, i + 1);
+    return isInt(s, pon, i + 1);
   }
 
+  /**
+   * Chama a funcao isReal com mais parametros
+   * 
+   * @param s -> string a ser testada
+   * @return -> true ou false
+   */
   public static boolean isReal(String s) {
-    int vir = 0, pon = 0;
+    if (isInt(s, 0, 0)) {
+      return true;
+    } else {
+      return isReal(s, 0, 0, 0);
+    }
+  }
 
-    if (!(isInt(s, 0))) {
-      for (int i = 0; i < s.length(); i++) {
-        if (s.charAt(i) == ',') {
-          vir++;
-          if (vir > 1) {
-            return false;
-          }
-        } else if (s.charAt(i) == '.') {
-          pon++;
-          if (pon > 1) {
-            if (s.charAt(i - 1) < 48 || s.charAt(i - 1) > 57) {
-              return false;
-            } else if (s.charAt(i - 2) < 48 || s.charAt(i - 2) > 57) {
-              return false;
-            } else if (s.charAt(i - 3) < 48 || s.charAt(i - 3) > 57) {
-              return false;
-            } else if (s.charAt(i - 4) != '.') {
-              return false;
-            }
-          } else if (vir > 0) {
-            return false;
-          }
-        } else if (s.charAt(i) < 48 || s.charAt(i) > 57) {
-          return false;
-        }
-      }
+  /**
+   * Testa se a string eh um numero real
+   * 
+   * @param s   -> string a ser testada
+   * @param i   -> posicao da string
+   * @param vir -> contador de virgulas da string
+   * @param pon -> contador de pontos da string
+   * @return -> true ou false
+   */
+  public static boolean isReal(String s, int i, int vir, int pon) {
 
+    if (i == s.length()) {
       if (vir == 1) {
         return true;
       } else if (pon > 1) {
         return false;
-      } else {
+      }else if(pon == 1){
         return true;
       }
     }
+    if (s.charAt(i) == ',') {
+      vir++;
+      if (vir > 1) { // se houver mais de uma virgula, nao eh real
+        return false;
+      }
+    } else if (s.charAt(i) == '.') {
+      pon++;
+      if (pon > 1) { // se houver mais de um ponto, o segundo deve ser precedido de 3 numeros e outro
+                     // ponto
+        if (s.charAt(i - 1) < 48 || s.charAt(i - 1) > 57) {
+          return false;
+        } else if (s.charAt(i - 2) < 48 || s.charAt(i - 2) > 57) {
+          return false;
+        } else if (s.charAt(i - 3) < 48 || s.charAt(i - 3) > 57) {
+          return false;
+        } else if (s.charAt(i - 4) != '.') {
+          return false;
+        }
+      } 
+    } else if (s.charAt(i) < 48 || s.charAt(i) > 57) {// se nao for um numero nao eh real
+      return false;
+    }
+    return isReal(s, i + 1, vir, pon);
 
-    return true;
   }
 }
